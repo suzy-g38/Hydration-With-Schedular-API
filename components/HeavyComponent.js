d31b040c397138eddb7f854789c0a39cb8cf549f
sulagna ghosh <sulagnaghosh52.github@gmail.com>
2025-02-27 01:49:07 +0530

    updated code

--- components/HeavyComponent.js
+++ components/HeavyComponent.js
@@ -1,94 +1,3 @@
-//Normal-Hydration
-
-// import React, { useState, useEffect } from "react";
-
-// const HeavyComponent = () => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     const start = performance.now();
-//     while (performance.now() - start < 50) {} // Simulate heavy work
-//     console.log("Hydrated HeavyComponent");
-//   }, []);
-
-//   return (
-//     <button onClick={() => setCount(count + 1)}>
-//       Click Me! Count: {count}
-//     </button>
-//   );
-// };
-
-// export default HeavyComponent;
-
-//Selective Hydration
-
-// import React, { useState, useEffect } from "react";
-
-// const HeavyComponent = ({ defer }) => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     const hydrate = () => {
-//       const start = performance.now();
-//       while (performance.now() - start < 50) {} // Simulate heavy work
-//       console.log("Hydrated HeavyComponent");
-//     };
-
-//     if (defer) {
-//       setTimeout(hydrate, 2000); // Delay hydration
-//     } else {
-//       hydrate();
-//     }
-//   }, [defer]);
-
-//   return (
-//     <button onClick={() => setCount(count + 1)}>
-//       Click Me! Count: {count}
-//     </button>
-//   );
-// };
-
-// export default HeavyComponent;
-
-//Progressive Hydration + Scheduler
-
-// import React, { useState, useEffect } from "react";
-
-// const scheduleTask = (task) => {
-//   if ("scheduler" in window) {
-//     window.scheduler.postTask(task, { priority: "background" });
-//   } else {
-//     requestIdleCallback(task);
-//   }
-// };
-
-// const HeavyComponent = ({ defer }) => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     const hydrate = () => {
-//       const start = performance.now();
-//       while (performance.now() - start < 50) {} // Simulate heavy work
-//       console.log("Hydrated Optimized HeavyComponent");
-//     };
-
-//     if (defer) {
-//       scheduleTask(hydrate);
-//     } else {
-//       hydrate();
-//     }
-//   }, [defer]);
-
-//   return (
-//     <button onClick={() => setCount(count + 1)}>
-//       Click Me! Count: {count}
-//     </button>
-//   );
-// };
-
-// export default HeavyComponent;
-
-
 import React, { useState, useEffect } from "react";
 
 const scheduleTask = (task) => {
@@ -105,7 +14,7 @@ const HeavyComponent = ({ defer }) => {
   useEffect(() => {
     const hydrate = () => {
       const start = performance.now();
-      while (performance.now() - start < 50) {} // Simulate heavy work
+      while (performance.now() - start < 50) {} 
       console.log("Hydrated Optimized HeavyComponent");
     };
 
@@ -124,49 +33,3 @@ const HeavyComponent = ({ defer }) => {
 };
 
 export default HeavyComponent;
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-
-// const scheduleTask = (task) => {
-//   if ("scheduler" in window) {
-//     // @ts-ignore
-//     window.scheduler.postTask(task, { priority: "background" });
-//   } else {
-//     requestIdleCallback(task);
-//   }
-// };
-
-// const HeavyComponent = ({ defer }) => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     const hydrate = () => {
-//       const start = performance.now();
-//       while (performance.now() - start < 50) {} // Simulate heavy work
-
-//       console.log("Hydrated Optimized HeavyComponent");
-//     };
-//     hydrate();
-
-//     if (defer) {
-//       // scheduleTask(hydrate);
-//     } else {
-//       // hydrate();
-//     }
-//   }, [defer]);
-
-//   return (
-//     <button onClick={() => setCount(count + 1)}>
-//       Click Me! Count: {count}
-//     </button>
-//   );
-// };
-
-// export default HeavyComponent;
-
-
-
